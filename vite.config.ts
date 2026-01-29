9a84e036ca866a0f21c122a770f9b240a27d1250
japason <s.tanaka0502@gmail.com>
2026-01-29 15:43:32 +0900

    Refactor vite.config.ts to enable React plugin and configure server settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173
-//   }
-// })
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173
+  }
+})
 
 //githubで公開するための設定
-export default defineConfig({
-  base: "/fe-pubsite-questions/",
-})
+// export default defineConfig({
+//   base: "/fe-pubsite-questions/",
+// })
 
 ;
 
